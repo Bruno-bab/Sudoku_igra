0841418d5864b17ff60e97f567341c0814588b0f
Bruno B. <brbaburi@vsite.hr>
2025-07-01 21:30:44 +0200

    Dodano da su upisani brojevi obojeni, na klik na broj ostali isti brojevi se označe, kliknuti broj je posebno označen

--- Sudoku_igra.cpp
+++ Sudoku_igra.cpp
@@ -1,9 +1,10 @@
 #include "Number_generator.h"
 #include "Sudoku_igra.h"
 
-std::vector<sudoku_button> sudoku_buttons;
-std::vector<number_button> number_buttons;
+vector<sudoku_button> sudoku_buttons;
+vector<number_button> number_buttons;
 std::string selected_number = "";
+bool same_numbers = false;
 
 int selected_sudoku_id = -1;
 int selected_number_id = -1;
@@ -16,8 +17,8 @@ void on_create(HWND hw)
 void on_create_game(HWND hw)
 {
 	srand(time(0));
-	int k = 30;
-	vector<vector<int>> sudoku = sudokuGenerator(k);
+	int numbers_to_remove = 30;
+	vector<vector<int>> sudoku = sudokuGenerator(numbers_to_remove);
 	int y = 50;
 	int id = 0;
 	for (int i = 0; i < 9; i++)
@@ -56,31 +57,99 @@ void on_create_game(HWND hw)
 	}
 }
 
+void on_paint(HWND hw)
+{
+	PAINTSTRUCT ps;
+	HDC hdc = BeginPaint(hw, &ps);
+	HBRUSH brush = CreateSolidBrush(RGB(0, 0, 0));
+	RECT r = { 318, 118, 963, 763 };
+	FillRect(hdc, &r, brush);
+
+	DeleteObject(brush);
+
+	EndPaint(hw, &ps);
+}
+
 void on_command(HWND hw, int id)
 {
 	if (id >= 0 && id < 81)
 	{
 		if (selected_sudoku_id != -1)
+			sudoku_buttons[selected_sudoku_id].setClickedButton(false);
+
+		for (int i = 0; i < 81; i++)
 		{
-			SendMessage(sudoku_buttons[selected_sudoku_id].getHWND(), BM_SETCHECK, BST_UNCHECKED, 0);
+			sudoku_buttons[i].setSameNumber(false);
+			if (sudoku_buttons[i].getText() == sudoku_buttons[id].getText() && sudoku_buttons[id].getText() != "" && id != i)
+			{
+				sudoku_buttons[i].setSameNumber(true);
+				HWND button = GetDlgItem(hw, sudoku_buttons[i].getId());
+				InvalidateRect(button, 0, true);
+			}
 		}
 
 		selected_sudoku_id = id;
+		sudoku_buttons[selected_sudoku_id].setClickedButton(true);
+
+		HWND button = GetDlgItem(hw, sudoku_buttons[selected_sudoku_id].getId());
+		InvalidateRect(button, 0, true);
 	}
+	
 	if (id >= 101 && id <= 109)
 	{
 		selected_number_id = id;
-	}
 
-	if (selected_sudoku_id != -1 && selected_number_id != -1 && sudoku_buttons[selected_sudoku_id].getText() == "")
-	{
-		std::string val = number_buttons[selected_number_id - 101].getText();
-		sudoku_buttons[selected_sudoku_id].setText(val);
-		sudoku_buttons[selected_sudoku_id].setHighlighted(true);
-		InvalidateRect(sudoku_buttons[selected_sudoku_id].getHWND(), 0, true);
-		SendMessage(sudoku_buttons[selected_sudoku_id].getHWND(), BM_SETCHECK, BST_UNCHECKED, 0);
+		if (selected_sudoku_id != -1 && sudoku_buttons[selected_sudoku_id].getText() == "")
+		{
+			std::string val = number_buttons[selected_number_id - 101].getText();
+			sudoku_buttons[selected_sudoku_id].setText(val);
+			sudoku_buttons[selected_sudoku_id].setEnteredNumber(true);
+
+			HWND button = GetDlgItem(hw, sudoku_buttons[selected_sudoku_id].getId());
+			InvalidateRect(button, 0, true);
+		}
 	}
 	selected_number_id = -1;
+}
+
+void on_drawitem(LPARAM lp)
+{
+	LPDRAWITEMSTRUCT dis = (LPDRAWITEMSTRUCT)lp;
+
+	for (int i = 0; i < 81; i++)
+	{
+		if (dis->CtlID == sudoku_buttons[i].getId())
+		{
+
+			bool isPressed = (dis->itemState & ODS_SELECTED);
+			bool isFocused = (dis->itemState & ODS_FOCUS);
+
+			COLORREF button_color = RGB(240, 240, 240);
+
+			if (sudoku_buttons[i].getClickedButton())
+				button_color = RGB(190, 190, 230);
+
+			if (sudoku_buttons[i].getSameNumber())
+				button_color = RGB(220, 220, 235);
+
+			HBRUSH brush = CreateSolidBrush(button_color);
+			FillRect(dis->hDC, &dis->rcItem, brush);
+
+			DrawEdge(dis->hDC, &dis->rcItem, EDGE_RAISED, BF_RECT);
+
+			if (sudoku_buttons[i].getEnteredNumber())
+				SetTextColor(dis->hDC, RGB(0, 0, 255));
+			SetBkMode(dis->hDC, TRANSPARENT);
+
+			RECT button_text = dis->rcItem;
+			if (isPressed)
+				OffsetRect(&button_text, 1, 1);
+
+			DrawText(dis->hDC, sudoku_buttons[i].getText().c_str(), -1, &button_text,
+				DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+			DeleteObject(brush);
+		}
+	}
 }
 
 void on_destroy() 
@@ -92,27 +161,18 @@ LRESULT CALLBACK window_proc(HWND hw, UINT msg, WPARAM wp, LPARAM lp)
 {
 	switch (msg)
 	{
-	case WM_CREATE:
-	{
-		on_create_game(hw);
-		return 0;
-	}
-		case WM_PAINT:
-		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(hw, &ps);
-			HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
-			RECT rect = { 318, 118, 963, 763 }; 
-			FillRect(hdc, &rect, hBrush);
-
-			DeleteObject(hBrush);
-			EndPaint(hw, &ps);
+		case WM_CREATE:
+			on_create_game(hw);
 			return 0;
-		}
+		case WM_PAINT:
+			on_paint(hw);
+			return 0;
 		case WM_COMMAND:
 			on_command(hw, LOWORD(wp));
 			return 0;
-		break;
+		case WM_DRAWITEM:
+			on_drawitem(lp);
+			return 0;
 		case WM_DESTROY:
 			on_destroy();
 			return 0;
--- Sudoku_igra.h
+++ Sudoku_igra.h
@@ -6,33 +6,54 @@
 class sudoku_button
 {
 private:
-    bool is_highlighted = false;
+    bool entered_number = false;
+    bool clicked_button = false;
+    bool same_number = false;
 	HWND hwnd = nullptr;
 	int id;
 	string text;
 public:
-    sudoku_button(HWND hw, int x, int y, int w, int h, int controlId, const string& txt)
-        : id(controlId), text(txt)
+    sudoku_button(HWND hw, int x, int y, int w, int h, int ID, const string& txt): id(ID), text(txt)
     {
-        hwnd = CreateWindow("BUTTON", txt.c_str(), WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX | BS_PUSHLIKE, x, y, 70, 70, hw, HMENU(controlId), 0, 0);
+        hwnd = CreateWindow("BUTTON", "", WS_CHILD | WS_VISIBLE | BS_OWNERDRAW,
+            x, y, 70, 70, hw, HMENU(ID), 0, 0);
     }
 
-    void setText(const string& txt) {
+    void setText(const string& txt)
+    {
         text = txt;
-        SetWindowText(hwnd, text.c_str());
+        InvalidateRect(hwnd, nullptr, TRUE);
     }
 
-    void setHighlighted(bool state) { is_highlighted = state; }
+    void setEnteredNumber(bool en)
+    {
+        entered_number = en;
+        InvalidateRect(hwnd, nullptr, TRUE);
+    }
 
-    bool isHighlighted() const { return is_highlighted; }
+    void setClickedButton(bool cb)
+    {
+        clicked_button = cb;
+        InvalidateRect(hwnd, nullptr, TRUE);
+    }
+
+    void setSameNumber(bool sn)
+    {
+        same_number = sn;
+        InvalidateRect(hwnd, nullptr, TRUE);
+    }
+
+    bool getEnteredNumber() const { return entered_number; }
+
+    bool getClickedButton() const { return clicked_button; }
+
+    bool getSameNumber() const { return same_number; }
 
     string getText() { return text; }
 
     HWND getHWND() { return hwnd; }
 
-    void Enable(bool enable = true) { EnableWindow(hwnd, enable); }
-
-    void Show(bool show = true) { ShowWindow(hwnd, show ? SW_SHOW : SW_HIDE); }
+    int getId() const { return id; }
 
 };
 
@@ -41,15 +62,15 @@ class number_button
 private:
     HWND hwnd = nullptr;
     int id;
-    string label;
+    string text;
 public:
-    number_button(HWND hw, int x, int y, int width, int height, int controlId, const string& text): id(controlId), label(text)
+    number_button(HWND hw, int x, int y, int width, int height, int ID, const string& txt): id(ID), text(txt)
     {
-        hwnd = CreateWindow("BUTTON", text.c_str(), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-            x, y, 70, 70, hw, HMENU(controlId), 0, 0);
+        hwnd = CreateWindow("BUTTON", txt.c_str(), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            x, y, 70, 70, hw, HMENU(ID), 0, 0);
     }
 
-    string getText() { return label; }
+    string getText() { return text; }
     
     HWND getHWND() { return hwnd; }
 
